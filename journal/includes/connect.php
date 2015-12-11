365b85af7c8174aeb585d47e78cd4b830199b64f
andriyrusyn <rusyn.andriy@gmail.com>
2015-12-11 09:36:58 -0500

    bg and sql fixes

--- journal/includes/connect.php
+++ journal/includes/connect.php
@@ -4,7 +4,13 @@
 	$db_user = "root"; // Enter your username
 	$db_pass = "password"; // Enter your password
 	
+	mysqli_connect("localhost", "root","password","426project");
 
-	mysql_connect($server, $db_user, $db_pass) or die("Could not connect to server!");
-	mysql_select_db($db_name) or die("Could not connect to database!");
+	// Check connection
+	if (mysqli_connect_errno())
+	  {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }
+	// mysqli_connect($server, $db_user, $db_pass, $db_name) or die("Could not connect to server!");
+	// mysqli_select_db($db_name) or die("Could not connect to database!");
 ?>
